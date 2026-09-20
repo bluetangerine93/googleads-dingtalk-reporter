68cc2852c6e25b1df847c64989f704b5339b7a8e
bluetangerine93 <bluetangerine93@users.noreply.github.com>
2026-09-20 16:02:08 +0800

    Fix JaiFinance Adjust campaign mapping

--- googleads_dingtalk/config.py
+++ googleads_dingtalk/config.py
@@ -156,7 +156,7 @@ def load_settings() -> Settings:
         ),
         adjust_facebook_channels=parse_csv(env("ADJUST_FACEBOOK_CHANNELS", "Facebook")),
         adjust_facebook_account_patterns=parse_named_patterns(
-            env("ADJUST_FACEBOOK_ACCOUNT_PATTERNS", "PocketMitra-03:pocketmitra_03,JaiFinance-01:pocketmitra_01_new")
+            env("ADJUST_FACEBOOK_ACCOUNT_PATTERNS", "PocketMitra-03:pocketmitra_03,JaiFinance-01:pocketmitra_new_01")
         ),
         fb_access_token=env("FB_ACCESS_TOKEN", env("FB_TOKEN")),
         fb_api_version=env("FB_API_VERSION", "v19.0"),
